9ebb4cfa124257ac212486c65aa41b8198a6ba94
Hex <hex@codeigniter.org.cn>
2017-01-24 19:19:25 +0800

    init commit

---
+++ spec/tree-view-sort-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TreeViewSortView from '../lib/tree-view-sort-view';
+
+describe('TreeViewSortView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
